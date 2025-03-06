2fc5c705705babad63bd10320ae3d5d14e93107c
t-yamamoto-being <t-yamamoto@beingcorp.co.jp>
2025-03-06 03:18:03 +0000

    bug_cafe_提出2回目

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+order_drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+order_food = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[order_drink][:price].to_i + FOODS[order_food][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
